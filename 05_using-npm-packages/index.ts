774daafa57bd5fefffa8d4e2dc0e9e47d6c19dc0
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-06-12 08:44:05 +0400

    Improve using npm packages code

--- 05_using-npm-packages/index.ts
+++ 05_using-npm-packages/index.ts
@@ -3,22 +3,12 @@ import { createRequire } from "https://deno.land/std/node/module.ts";
 const require = createRequire(import.meta.url);
 
 // Moment
-// const moment = require("moment");
-// // in relation to release date of this post
-// console.log(moment().format("MMMM Do YYYY")); // June 6th 2019
-// moment("20111031", "YYYYMMDD").fromNow(); // 8 years ago
-// moment().subtract(10, "days").calendar(); // 05/27/2019
+const moment = require("moment");
+console.log(moment().format("MMMM Do YYYY"));
+console.log(moment("20111031", "YYYYMMDD").fromNow());
+console.log(moment().subtract(10, "days").calendar());
 
 // Lodash
 const _ = require("lodash");
-console.log(_.defaults({ a: 1 }, { a: 3, b: 2 })); // { 'a': 1, 'b': 2 }
-_.partition([1, 2, 3, 4], (n: any) => n % 2); // [[1, 3], [2, 4]]
-
-// request
-const request = require("request");
-
-request("http://www.google.com", (error: any, response: any, body: any) => {
-  console.log("error:", error);
-  console.log("statusCode:", response && response.statusCode);
-  console.log("body:", body);
-});
+console.log(_.defaults({ a: 1 }, { a: 3, b: 2 }));
+console.log(_.partition([1, 2, 3, 4], (n: any) => n % 2));
