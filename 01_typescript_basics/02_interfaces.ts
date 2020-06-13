2e0457aaec07ce1d2e0727e39c2350eb9c370b8f
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-06-13 11:47:55 +0400

    remove comments from typescript folder

--- 01_typescript_basics/02_interfaces.ts
+++
@@ -1,9 +0,0 @@
-// Implicit object types
-
-// Explicit object types
-
-// With Dynamic properties
-
-// Object types in function arguments
-
-// Interfaces, with readonly properties, dynamic keys, methods
--- 01_typescript_basics/04_generics.ts
+++
@@ -1,7 +0,0 @@
-// Simple example of generics using Map
-
-// Class extends from Map
-
-// Create getPromise function with Generics
-
-// Generics with T extends Map
